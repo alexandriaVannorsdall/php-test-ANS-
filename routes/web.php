c7cbe2ac781e0e0ead32df19d9cfd1b10cb4777e
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2024-06-24 13:49:05 +0100

    refactor: removed authentication since it was never set up

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonController;
 
 Route::get('/', [PokemonController::class, 'index'])
-    ->middleware(['auth'])
     ->name('pokemon.index');
 
 Route::get('/pokemon/{name}', [PokemonController::class, 'show'])
-    ->middleware(['auth'])
     ->name('pokemon.show');
